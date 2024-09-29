e0045e2d620d8db1221f8f42f91c821b90c44b8b
Ahmad Alhseen <ahmed.23chatgpt@gmail.com>
2024-09-29 10:52:25 +0300

    fix the error

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,7 +9,7 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request,Closure $next)
+    protected function redirectTo(Request $request)
     {
 
     return  $request->expectsJson() ? null : route('login');
